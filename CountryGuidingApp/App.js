8a2aa6a956e86db2efbd2af6b805fb0171e0491a
Shafali Gupta <43209217+shafali123@users.noreply.github.com>
2024-02-22 20:31:43 +0530

    Add files via upload

---
+++ CountryGuidingApp/App.js
@@ -0,0 +1,14 @@
+// App.js 
+
+import './App.css'; 
+import CountryInformation from './components/CountryInformation'; 
+
+function App() { 
+return ( 
+	<div className="App"> 
+	<CountryInformation /> 
+	</div> 
+); 
+} 
+
+export default App; 
